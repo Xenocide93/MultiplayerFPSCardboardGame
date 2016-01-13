bb943f4a3dc8b7d7ab2598ea59801d01b48dd052
Ong Patinya Petcharit <ppong1993@hotmail.com>
2016-01-13 14:48:43 +0700

    add terrain, house, edit some light and sun, move things around.

---
+++ Assets/Medieval_House/Scripts/OpenDoor.js
@@ -0,0 +1,18 @@
+﻿#pragma strict
+var doorObject:GameObject;
+var openedDoor:boolean;
+function Start () {
+	openedDoor=true;
+}
+
+function Update () {
+	
+}
+
+function OnTriggerEnter(other:Collider){
+	if (openedDoor){
+		doorObject.GetComponent.<Animation>().Play();
+		openedDoor=false;
+		Debug.Log("open");
+	}
+}
