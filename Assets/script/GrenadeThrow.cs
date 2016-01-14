c4f734dd25e0d7b701267c34cde362bedec613bd
violinisms <sakumo_whitefang@hotmail.com>
2016-01-14 21:54:28 +0700

    add grenade explosion effect

--- Assets/script/GrenadeThrow.cs
+++ Assets/script/GrenadeThrow.cs
@@ -6,10 +6,19 @@ public class GrenadeThrow : MonoBehaviour {
 	public float range = 20f;
 	public float damage = 70f;
 	public float detonateDelay = 3f;
+	public GameObject explosionEffectObject;
 
 	private float timer = 0f;
 	private Rigidbody rigidbody;
 	private bool isDetonated = false;
+
+	private GameObject explosionEffectObjectClone;
+	private ParticleSystem explosionEffectParticle;
+	private Light explosionEffectLight;
+	private float destroy_timer = 0f;
+	private bool isDestroy = false;
+
+
 
 	// Use this for initialization
 	void Start () {
@@ -19,14 +28,41 @@ public class GrenadeThrow : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		timer += Time.deltaTime;
-		if (timer >= detonateDelay) {
+		if (timer >= detonateDelay && !isDestroy) {
 			detonate ();
 		}
+		if (isDestroy) {
+			destroy_timer += Time.deltaTime;
+			if (destroy_timer >= 1f)
+				destroy ();
+		}
+	}
+
+	private void destroy(){
+		Destroy (gameObject);
+		Destroy (explosionEffectObjectClone);
+		isDestroy = false;
+		destroy_timer = 0f;
 	}
 
 	public void detonate() {
 		isDetonated = true;
+		isDestroy = true;
 		Collider[] colliders = Physics.OverlapSphere (transform.position, range);
+
+		Vector3 grenadePosition = transform.position;
+		grenadePosition.y += 0.6f;
+
+		explosionEffectObjectClone = (GameObject) Instantiate(
+			explosionEffectObject, 
+			grenadePosition, 
+			transform.rotation
+		);
+		explosionEffectParticle = explosionEffectObjectClone.GetComponentInChildren <ParticleSystem> ();
+		explosionEffectLight = explosionEffectObjectClone.GetComponentInChildren <Light> ();
+		explosionEffectParticle.Play ();
+		explosionEffectLight.intensity = 3f;
+
 		foreach(Collider c in colliders){
 			if (c.GetComponent<Rigidbody>() == null) continue;
 			if (c.GetComponent<PlayerGameManager> () != null) {
@@ -39,7 +75,6 @@ public class GrenadeThrow : MonoBehaviour {
 			}
 			c.GetComponent<Rigidbody>().AddExplosionForce (damage, transform.position, range,0.0f,ForceMode.Impulse);
 		}
-		Destroy (gameObject);
 	}
 
 }
