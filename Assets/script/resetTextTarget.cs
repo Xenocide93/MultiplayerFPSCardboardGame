b48d3f8a9de66568746a13a8be352f1f53802255
Ong Patinya Petcharit <ppong1993@hotmail.com>
2016-01-14 13:55:43 +0700

    import tree assets, add reset testShootableTarget script << float ui button now fully works.

--- Assets/script/resetTextTarget.cs
+++ Assets/script/resetTextTarget.cs
@@ -2,14 +2,14 @@
 using System.Collections;
 
 public class resetTextTarget : MonoBehaviour {
+	public GameObject testShootPrefab;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+	public void resetTextShootObject(){
+		GameObject[] testShootTag = GameObject.FindGameObjectsWithTag ("TestShootTarget");
+		foreach (GameObject o in testShootTag) {
+			Destroy (o);
+		}
+
+		Instantiate (testShootPrefab, new Vector3(7.06f, 0.810892f, 0.57f), new Quaternion(0f, 0f, 0f, 0f));
 	}
 }
